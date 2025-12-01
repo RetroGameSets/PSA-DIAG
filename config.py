5a62b1074238d18794bf14f757fb019048ddcfda
skymike03 <mike.bosche@laposte.net>
2025-12-01 13:33:43 +0100

    v2.1.0.8 - create config.py for variable configuration - add versions in config.py - change log more verbose in integrated console

--- config.py
+++ config.py
@@ -3,21 +3,11 @@ import os
 
 # Central configuration used by main and build-time metadata
 CONFIG_DIR = Path(os.getenv('APPDATA', Path.home())) / 'PSA_DIAG'
-# Do NOT create the directory at import time; building with PyInstaller
-# imports this module during the spec execution and creating folders or
-# touching the filesystem can fail under some build contexts. Create the
-# directory lazily at runtime where needed (main.py already creates logs).
 
 # Application version (keep in sync with release tags)
-APP_VERSION = "2.1.0.7"
+APP_VERSION = "2.1.0.8"
 
 # Remote endpoints
 URL_LAST_VERSION_PSADIAG = "https://psa-diag.fr/diagbox/install/last_version_psadiag.json"
 URL_LAST_VERSION_DIAGBOX = "https://psa-diag.fr/diagbox/install/last_version_diagbox.json"
-# URL hosting a JSON file describing available Diagbox download options.
-# The JSON should be an array of objects like:
-# [
-#   {"display_name": "Diagbox 09.180 (2024)", "version": "09.180_PSA_DIAG", "url": "https://..."},
-#   ...
-# ]
 URL_VERSION_OPTIONS = "https://psa-diag.fr/diagbox/install/available_versions.json"
